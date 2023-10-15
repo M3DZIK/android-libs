248db67f964470ee6f95c8d7d142faa6cec8b3e0
M3DZIK <me@medzik.dev>
2023-10-15 20:16:06 +0200

    Release v1.2.0

--- buildSrc/src/main/kotlin/dev/medzik/gradle/DeployGradlePlugin.kt
+++ buildSrc/src/main/kotlin/dev/medzik/gradle/DeployGradlePlugin.kt
@@ -27,7 +27,7 @@ fun Project.publishConfig(
         publications {
             create<MavenPublication>("mavenJava") {
                 groupId = "dev.medzik.android"
-                version = "1.1.0"
+                version = "1.2.0"
 
                 configuration()
 

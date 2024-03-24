512df28d13e445b4d9694876487543b6fd33abed
M3DZIK <me@medzik.dev>
2024-03-24 12:17:54 +0100

    Add Preview to the LoadingDot

--- components/src/main/java/dev/medzik/android/components/LoadingDot.kt
+++ components/src/main/java/dev/medzik/android/components/LoadingDot.kt
@@ -2,14 +2,19 @@ package dev.medzik.android.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 /**
- * Only the dot that is used in [LoadingIndicator].
+ * The dot that is used in [LoadingIndicator].
  *
  * @param color The color of the dot.
  * @param modifier The modifier to apply to the dot.
@@ -21,8 +26,20 @@ fun LoadingDot(
 ) {
     Box(
         modifier =
-        modifier
-            .clip(CircleShape)
-            .background(color)
+            modifier
+                .clip(CircleShape)
+                .background(color)
+    )
+}
+
+@Preview
+@Composable
+fun LoadingDotPreview() {
+    LoadingDot(
+        color = MaterialTheme.colorScheme.primary,
+        modifier =
+            Modifier
+                .width(8.dp)
+                .aspectRatio(1f)
     )
 }

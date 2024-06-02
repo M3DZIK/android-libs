31996a8b4d57d6d03dcfc3e35c940f252ab31306
Oskar Karpiński <me@medzik.dev>
2024-06-02 22:15:58 +0200

    Update docs of KeyStoreAlias interface

--- crypto/src/main/java/dev/medzik/android/crypto/KeyStoreAlias.kt
+++ crypto/src/main/java/dev/medzik/android/crypto/KeyStoreAlias.kt
@@ -1,8 +1,13 @@
 package dev.medzik.android.crypto
 
 /**
- * KeyStore alias for keystore operations.
+ * Interface representing a keystore alias for keystore operations.
+ *
+ * This interface should be implemented by object or enum that represents a keystore alias.
  */
 interface KeyStoreAlias {
+    /**
+     * KeyStore alias name.
+     */
     val name: String
 }

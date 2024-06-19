888b45df5791f28ba8bde62b60d56d0db1dcd333
Oskar Karpiński <me@medzik.dev>
2024-06-19 18:49:29 +0200

    Add preview for `PreferenceGroupTitle`

--- compose/src/main/java/dev/medzik/android/compose/ui/preference/PreferenceGroupTitle.kt
+++ compose/src/main/java/dev/medzik/android/compose/ui/preference/PreferenceGroupTitle.kt
@@ -1,8 +1,10 @@
 package dev.medzik.android.compose.ui.preference
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import dev.medzik.android.compose.ui.GroupTitle
 
@@ -20,3 +22,17 @@ fun PreferenceGroupTitle(
     text = title,
     modifier = modifier.padding(vertical = 4.dp, horizontal = 16.dp)
 )
+
+@Preview(showBackground = true)
+@Composable
+private fun PreferenceGroupTitlePreview() {
+    Column {
+        PreferenceGroupTitle("Group title")
+
+        SwitcherPreference(
+            title = "Switcher Preference",
+            checked = false,
+            onCheckedChange = {}
+        )
+    }
+}

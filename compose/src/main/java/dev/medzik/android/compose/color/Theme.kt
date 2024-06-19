4e7c3f56c13686a5b953e39bae7ad8fcb7d5894d
Oskar Karpiński <me@medzik.dev>
2024-06-19 17:08:45 +0200

    Add Previews for colors

--- compose/src/main/java/dev/medzik/android/compose/color/Theme.kt
+++ compose/src/main/java/dev/medzik/android/compose/color/Theme.kt
@@ -1,9 +1,16 @@
 package dev.medzik.android.compose.color
 
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ColorScheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.compositeOver
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 private const val SUCCESS = 120f
 private const val INFO = 210f
@@ -50,3 +57,42 @@ val ColorScheme.infoContainer: Color
 val ColorScheme.warningContainer: Color
     @Composable
     get() = buildContainerColor(WARNING)
+
+@Preview
+@Composable
+fun SuccessContainerPreview() {
+    Surface(
+        color = MaterialTheme.colorScheme.successContainer,
+        shape = MaterialTheme.shapes.large
+    ) {
+        Spacer(
+            modifier = Modifier.padding(horizontal = 30.dp, vertical = 15.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun InfoContainerPreview() {
+    Surface(
+        color = MaterialTheme.colorScheme.infoContainer,
+        shape = MaterialTheme.shapes.large
+    ) {
+        Spacer(
+            modifier = Modifier.padding(horizontal = 30.dp, vertical = 15.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun WarningContainerPreview() {
+    Surface(
+        color = MaterialTheme.colorScheme.warningContainer,
+        shape = MaterialTheme.shapes.large
+    ) {
+        Spacer(
+            modifier = Modifier.padding(horizontal = 30.dp, vertical = 15.dp)
+        )
+    }
+}
